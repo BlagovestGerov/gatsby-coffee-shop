6fe4084897a8c5717909602518defc82cd045904
BlagovestGerov <blaje@abv.bg>
2019-07-14 16:25:12 +0300

    added navbar and toggler button

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,9 +10,13 @@ import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 import './bootstrap.min.css';
 import "./layout.css";
+import Navbar from './Globals/Navbar';
+
 
 const Layout = ({ children }) => {
- return (<>
+ return (
+<>
+<Navbar />
  {children}
  </>)
 }
