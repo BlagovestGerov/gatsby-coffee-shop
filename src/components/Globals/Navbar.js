2652bc8c79af7cfdba03d6d3028aaeafafa149e2
BlagovestGerov <blaje@abv.bg>
2019-07-14 16:36:04 +0300

    added home and about navbars

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby';
 import logo from '../../images/logo.svg';
-// import {FaCartArrowDown} from 'react-icons/fa';
+import {FaCartArrowDown} from 'react-icons/fa';
 
 export default class Navbar extends Component {
     state={
@@ -39,6 +39,24 @@ https://www.iconfinder.com/webalys */}
        onClick={this.navbarHandler}>
            <span className="navbar-toggler-icon"></span>
        </button>
+       <div className={this.state.css}>
+            <ul className="navbar-nav mx-auto">
+               {
+                   this.state.links.map(link => {
+                   return (
+                    <li key={link.id} className="nav-item">
+                        <Link to={link.path} className="nav-link text-capitalize">
+                        {link.text}
+                        </Link>
+                    </li>
+                   )
+                   })
+               }
+               <li className="nav-item ml-sm-5">
+                <FaCartArrowDown className="cart-icon" />
+               </li>
+            </ul>
+       </div>
         </nav>
         );
       
