a6360a9e9724cb1202e43a664d418e3e1a454db2
BlagovestGerov <blaje@abv.bg>
2019-07-20 23:34:20 +0300

    Added email contact form using Formspree

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import SEO from "../components/seo"
 import BackgroundSection from '../components/Globals/BackgroundSection';
 import Info from '../components/Home/Info';
 import Menu from '../components/Home/Menu';
+import Contact from '../components/Home/Contact';
 
 const IndexPage = ({data}) => (
   <Layout>
@@ -16,6 +17,7 @@ const IndexPage = ({data}) => (
  />
  <Info />
  <Menu items={data.menu} />
+ <Contact />
   </Layout>
 )
 
