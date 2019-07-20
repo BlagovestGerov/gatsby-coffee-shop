9eb468aed7d79e7284ad1e1a6636aa1fde672165
BlagovestGerov <blaje@abv.bg>
2019-07-20 12:14:09 +0300

    got access to menu data from contentful

---
+++ src/components/Home/Menu.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Menu extends Component {
+    constructor(props){
+        super(props)
+        console.log(props.items)
+    }
+    render() {
+        return (
+            <div>
+                contenftul menu data
+            </div>
+        )
+    }
+}
